ba4e057abc346dcfcd11c8d5083fbcabaf7fa235
s-weberg <sandraweberg90@gmail.com>
2025-11-25 16:11:30 +0100

    updates

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 3000;
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/retro-games";
 
-// Middleware
 app.use(
   cors({
     origin: "http://localhost:5173",
